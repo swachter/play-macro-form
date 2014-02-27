3023bd5854c97e16dacfc98deb24124a8d362621
Stefan Wachter <stefan.wachter@gmx.de>
2014-02-27 12:50:54 +0100

    devel

--- play-app/app/eu/swdev/web/play/package.scala
+++ play-app/app/eu/swdev/web/play/package.scala
@@ -13,23 +13,23 @@ package object play {
 
   /**
    * Provides various methods for rendering fields.
-   *
-   * @param fieldState
-   * @tparam V
-   * @tparam M
-   * @tparam CS
    */
-  implicit class FieldRenderer[V, M, CS <: CState](val fieldState: FieldState[V, M, CS]) extends AnyVal {
+  trait FieldRenderer[V, M, CS <: CState] {
 
-    def inputText(implicit bootstrapAttrs: BootstrapAttrs, lang: Lang): Html = {
+    implicit def bootstrapAttrs: BootstrapAttrs
+    implicit def lang: Lang
+    def fieldState: FieldState[V, M, CS]
+
+
+    def inputText: Html = {
       bootstrap3.input(fieldState, "text")
     }
 
-    def checkBox(implicit bootstrapAttrs: BootstrapAttrs, checkBoxValueInfo: CheckBoxValueInfo[V], lang: Lang): Html = {
+    def checkBox(implicit checkBoxValueInfo: CheckBoxValueInfo[V]): Html = {
       bootstrap3.checkBoxField(fieldState, checkBoxValueInfo)
     }
 
-    def checkBoxGroup(inLineBoxes: Boolean)(implicit bootstrapAttrs: BootstrapAttrs, lang: Lang, ev: CS <:< CState { type EN = IsSet; type OC = ZeroOrMore } ): Html = {
+    def checkBoxGroup(inLineBoxes: Boolean)(implicit ev: CS <:< CState { type EN = IsSet; type OC = ZeroOrMore } ): Html = {
       val checkBoxes = for {
         v <- fieldState.constraints.en.get.seq
       } yield {
@@ -40,7 +40,7 @@ package object play {
       bootstrap3.checkBoxOrRadioButtonGroup(fieldState, checkBoxes)
     }
 
-    def radioButtonGroup(inLineBoxes: Boolean)(implicit bootstrapAttrs: BootstrapAttrs, lang: Lang, ev: CS <:< CState { type EN = IsSet; type OC <: AtMostOne } ): Html = {
+    def radioButtonGroup(inLineBoxes: Boolean)(implicit ev: CS <:< CState { type EN = IsSet; type OC <: AtMostOne } ): Html = {
       val radioButtons = for {
         v <- fieldState.constraints.en.get.seq
       } yield {
@@ -52,16 +52,59 @@ package object play {
     }
 
   }
-  
-  implicit class FormRenderer[M](val formState: FormState[M]) {
 
-    def submit(implicit bootstrapAttrs: BootstrapAttrs, lang: Lang): Html = button("submit")
+  trait FormRenderer[M] {
 
-    def button(tpe: String = "submit")(implicit bootstrapAttrs: BootstrapAttrs, lang: Lang): Html = {
+    implicit def bootstrapAttrs: BootstrapAttrs
+    implicit def lang: Lang
+    def formState: FormState[M]
+
+    def submit: Html = button("submit")
+
+    def button(tpe: String = "submit"): Html = {
       val label = formUtil.findMessage(formState._name + "submit", "form.button").getOrElse(s"${formState._name}.submit")
       bootstrap3.button("submit", label)
     }
 
+  }
+
+  implicit class FieldRendererImpl[V, M, CS <: CState](val fieldState: FieldState[V, M, CS])(implicit val bootstrapAttrs: BootstrapAttrs, val lang: Lang) extends FieldRenderer[V, M, CS]
+
+  implicit class FormRendererImpl[M](val formState: FormState[M])(implicit val bootstrapAttrs: BootstrapAttrs, val lang: Lang) extends FormRenderer[M]
+
+  trait WithAttrs[R] {
+    def withAttrs(form: Attrs = null, formGroup: Attrs = null, label: Attrs = null, inputDiv: Attrs = null, input: Attrs = null, button: Attrs = null): R = {
+      val bsa = BootstrapAttrs(
+        form = sel(form, bootstrapAttrs.form),
+        formGroup = sel(formGroup, bootstrapAttrs.formGroup),
+        label = sel(label, bootstrapAttrs.label),
+        inputDiv = sel(inputDiv, bootstrapAttrs.inputDiv),
+        input = sel(input, bootstrapAttrs.input),
+        button = sel(button, bootstrapAttrs.button)
+      )
+      renderer(bsa)
+    }
+    def bootstrapAttrs: BootstrapAttrs
+    def renderer(bsa: BootstrapAttrs): R
+    def sel(attrs: Attrs, defaultAttrs: Attrs) = if (attrs != null) attrs else defaultAttrs
+  }
+
+  implicit class FieldWithAttrs[V, M, CS <: CState](val fieldStateArg: FieldState[V, M, CS])(implicit val bootstrapAttrs: BootstrapAttrs, val langArg: Lang) extends WithAttrs[FieldRenderer[V, M, CS]] {
+    def renderer(bsa: BootstrapAttrs)= new FieldRenderer[V, M, CS] {
+      override def fieldState = fieldStateArg
+      override implicit def lang: Lang = langArg
+      override implicit def bootstrapAttrs: BootstrapAttrs = bsa
+    }
+  }
+
+
+
+  implicit class FormWithAttrs[M](val formStateArg: FormState[M])(implicit val bootstrapAttrs: BootstrapAttrs, val langArg: Lang) extends WithAttrs[FormRenderer[M]] {
+    def renderer(bsa: BootstrapAttrs) = new FormRenderer[M] {
+      override def formState: FormState[M] = formStateArg
+      override implicit def lang: Lang = langArg
+      override implicit def bootstrapAttrs: BootstrapAttrs = bsa
+    }
   }
 
   implicit class FieldAttrs[V, M, CS <: CState](val fieldState: FieldState[V, M, CS]) extends AnyVal {
@@ -70,43 +113,6 @@ package object play {
     def nameForDefault(implicit bsa: BootstrapAttrs): String = bsa.input.map.getOrElse("name", Set()).headOption.getOrElse(fieldState.name.toString) + ".default"
   }
 
-
-//  class OutputAttrs(val map: Map[String, Set[String]]) extends AnyVal {
-//
-//    def id(fieldState: FieldState[_, _, _]): Html =
-//      attr("id", fieldState.name.toString)
-//
-//    def name(fieldState: FieldState[_, _, _]): Html =
-//      attr("name", fieldState.name.toString)
-//
-//    def nameForDefault(fieldState: FieldState[_, _, _]): String = {
-//      val n = map.getOrElse("name", Set()).headOption.getOrElse(fieldState.name.toString)
-//      s"$n.default"
-//    }
-//
-//    def value(fieldState: FieldState[_, _, _]): Html =
-//      attr("value", fieldState.view.headOption.getOrElse(""))
-//
-//    def `type`(inputType: String): Html =
-//      attr("type", inputType)
-//
-//    def `for`(fieldState: FieldState[_, _, _]): Html =
-//      attr("for", fieldState.name.toString)
-//
-//    def placeholder(fieldState: FieldState[_, _, _])(implicit lang: Lang): Html =
-//      attr("placeholder", formUtil.findMessage(fieldState.name, "form.placeholder").getOrElse(""))
-//
-//    private def attr(attrName: String, value: => String): Html = {
-//      if (map.contains(attrName)) {
-//        Html.empty
-//      } else {
-//        Html(s"""$attrName="$value"""")
-//      }
-//    }
-//
-//  }
-//
-//  implicit def toOutputAttrs(attrs: Attrs): OutputAttrs = new OutputAttrs(attrs.map)
 
   object formUtil {
 
